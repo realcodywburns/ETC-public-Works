1fb868aac76bc23b84534e184c28889f7afda154
Burns, Cody <cody.burns@accenture.com>
2018-10-29 15:18:46 -0500

    add validation and update error

--- Bridgette-bot/lib/tipper.js
+++ Bridgette-bot/lib/tipper.js
@@ -3,9 +3,15 @@ var dapp = require('../dapp');
 var auth = require('../auth');
 var botUnits = require('./botUnits');
 const bot = require('./discordbot');
-
-
+const Joi = require('joi');
 const crypto = require('crypto');
+
+//joi validation schema
+const schema = Joi.object().keys({
+  username: Joi.string().min(3).max(30).required(),
+  amount : Joi.number().integer().min(0).max(86753090).precision(0).required()
+}).with('username', 'amount');
+
 
 //contract
 const ABI = dapp.btoken.abi;
@@ -13,6 +19,7 @@ const ADDR = dapp.btoken.address;
 
 const tipper = new web3.eth.Contract(ABI, ADDR);
 
+//useful functions
 function hashStuff(dig){
    var hash = crypto.createHash('sha256');
    hash.update(dig);
@@ -29,51 +36,53 @@ async function addReactions(channelID, evt,emoji){
 
 module.exports = async (channelID, sender, senderID, args, evt ) => {
 
-
-//tipper send xxx <to>
-
+//format [tipper send xxx <to>]
 
  switch(args[0].toLowerCase()){
       case "send" :
         //add reaction to be polite
-        //addReactions(channelID, evt, '🤖');
+        addReactions(channelID, evt, '\u{1F916}');
 
         //convert to contract addreses
-        var _to = "0x" + hashStuff(args[1]);
+        var _to = "0x" + hashStuff(args[2]);
         var _from = "0x" + hashStuff('<@'+senderID+'>');
+
+        const result = Joi.validate({ username: args[2], amount: args[1] }, schema)
+        .catch(function(err){
+          addReactions(channelID, evt, "\u{1F6D1}");
+          console.log(err);
+        });
+
 
        //unlock the account and send the transaction
         //await web3.eth.personal.unlockAccount(auth.account, auth.passwd);
         //console.log('acct unlocked');
-        var gas = await tipper.methods.transfer(_from, _to, args[2]).estimateGas({from: auth.account});
+        var gas = await tipper.methods.transfer(_from, _to, args[1]).estimateGas({from: auth.account});
+
         //console.log(gas);
-        if(args[2] != null){
-          var amt = args[2];
-        } else {
-          var amt = args[3];
-        }
-        const msg = await tipper.methods.transfer(_from, _to, amt).send({
+
+        const msg = await tipper.methods.transfer(_from, _to, args[1]).send({
           from: auth.account,
           gas: Math.round(gas * 1.5),
           gasPrice: '50000000000'
         })
         .then(async function(receipt){
-          await addReactions(channelID, evt, '🤖');
+          await addReactions(channelID, evt, '\u{1F4B0}');
           console.log(receipt);
           }
          )
-         //⛏️
          .catch(function(err){
-           addReactions(channelID, evt, "⚠️");
+           addReactions(channelID, evt, "\u{1F6D1}");
            console.log(err);
          });
+
         return true;
 
         break;
 
     //* !tipper balance *//
       case "balance":
-        addReactions(channelID, evt, "🤖");
+        addReactions(channelID, evt, "\u{1F916}");
         if(args[1] == null){
          var account = "0x" + hashStuff("<@"+ senderID+">");
           } else {
@@ -82,7 +91,7 @@ module.exports = async (channelID, sender, senderID, args, evt ) => {
           await tipper.methods.balanceOf(account).call()
             .then( res => {
               //console.log(res);
-              addReactions(channelID, evt, "💰");
+              //addReactions(channelID, evt, "\u{1F4B0}");
               bot.sendMessage({
                  to: channelID,
                 message :  sender + ", the balance is "+ res
