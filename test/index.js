affe481c39349472bebcfd228aabd557e27e7eb7
Burns, Cody <cody.burns@accenture.com>
2018-10-30 13:19:35 -0500

    fix index test

--- test/index.js
+++ test/index.js
@@ -3,9 +3,12 @@ var assert = require('assert');
 //* unit tests *//
 
 var error = require('../Bridgette-bot/test/error');
+var error = require('../Bridgette-bot/test/donate');
+
 //* end unit tests *//
 
 /* tests */
 describe('Global functions', function() {
-  error;
+  error,
+  donated
 });
