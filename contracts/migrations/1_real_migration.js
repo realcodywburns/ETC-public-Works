2a7fb9bd6f533d2d5e268cf8fe72fd7a38126de9
Cody Burns <cody.w.burns@gmail.com>
2018-12-31 18:06:51 -0600

    Update 1_real_migration.js

--- contracts/migrations/1_real_migration.js
+++ contracts/migrations/1_real_migration.js
@@ -2,5 +2,5 @@ var Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
   // Deploy the Migrations contract as our only task
-  deployer.deploy(Migrations);
+ // deployer.deploy(Migrations);
 };
