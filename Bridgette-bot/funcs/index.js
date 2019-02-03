714758aad893290a1deab3b4d0b891ae1c078941
realcodywburns <cody.w.burns@gmail.com>
2019-02-03 08:54:12 -0600

    split funcs

---
+++ Bridgette-bot/funcs/index.js
@@ -0,0 +1,25 @@
+// common functions
+const { log } = require(../common);
+
+var getBlockNumber = require('./getblocknumber');
+var getBalance = require('./getBalance');
+var getTransaction = require('./getTransactions');
+var sendSignedTransaction = require('./sendSignedTransaction')
+var getGasPrice = require('./getGasPrice');
+var getBlock = require('./getBlock');
+var version = require('./version');
+
+log.info('[Bridgett-bot/index.js] functions loaded');
+
+module.exports = {
+    getBlockNumber = getBlockNumber,
+    getBalance = getBalance,
+    getTransaction = getTransaction,
+    sendSignedTransaction = sendSignedTransaction,
+    getGasPrice = getGasPrice,
+    getBlock = getBlock,
+    version = version,
+}
+
+// copy paste 
+// const { getBlockNumber, getBalance, getTransaction, sendSignedTransaction, getGasPrice, getBlock, version } = require( "./funcs" );
