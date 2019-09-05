2c9e2dd8750a6107e80b4eab5ff37b5316d6bb23
dontpanic <cody.w.burns@gmail.com>
2019-09-05 01:17:48 +0000

    working fork event

--- Bridgette-bot/dapps/atlantis.js
+++ Bridgette-bot/dapps/atlantis.js
@@ -1,7 +1,36 @@
-module.exports = (channelID, blockNumber) => {
+//atlantis fork block
+
+// 14 sec blocks
+
+// 
+
+function  getMsg( forkName, forkBlk, blocknumber ) {
+    var now = new Date().getTime();
+    var timeRemains = (forkBlk - blocknumber) * 14;
+    var countDownDate = ( timeRemains * 1000 )  + 604800 + new Date().getTime();
+    var distance = countDownDate - now;
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor( distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor( ( distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if(distance > 0){
+    var response = [
+        "The " + forkName +" update will happen in " + (forkBlk - blocknumber) + " blocks.",
+        "In about " + days + " days," + hours +" hours, " + minutes + " mins and " + seconds + " seconds",
+        "" + days + " days," + hours +" hours, " + minutes + " mins and " + seconds + " seconds",
+        "The  " + forkName + " fork will be in " + days + " days," + hours +" hours, " + minutes + " mins and " + seconds + " seconds" 
+	];
+    return response[Math.floor(Math.random() * response.length)];
+    } else {
+	return ""+ forkName + " is here!!!!";
+}
+}
+
+
+module.exports = (channelID, forkName, forkBlk, blockNumber) => {
 return{
   to: channelID,
-  message :  "The current block height is: " + blockNumber + "\n"
-  + " There are " + (8772000 - blockNumber) + " blocks until the Atlantis fork"
+  message : getMsg( forkName, forkBlk, blockNumber )
   };
 };
